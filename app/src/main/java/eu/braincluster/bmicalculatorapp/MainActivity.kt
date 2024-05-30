53ecf0850dce0e8f880b9f07a0e61179515802aa
Török Viktor <viktor.torok@hungeritzrt.hu>
2024-05-30 14:40:29 +0200

    Developing the app

--- app/src/main/java/eu/braincluster/bmicalculatorapp/MainActivity.kt
+++ app/src/main/java/eu/braincluster/bmicalculatorapp/MainActivity.kt
@@ -1,20 +1,24 @@
 package eu.braincluster.bmicalculatorapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import eu.braincluster.bmicalculatorapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.getRoot());
+
+        binding.textViewHeight.text = "${binding.sliderHeight.value.toInt().toString()} cm"
+
+        binding.sliderHeight.addOnChangeListener { slider, value, fromUser ->
+            binding.textViewHeight.text = "${value.toInt().toString()} cm"
         }
     }
 }
